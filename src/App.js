97fb08b6edf7cc22c14a3d135f8f9800410e4d47
Ju777 <julien.tamil@gmail.com>
2022-08-18 16:44:10 +0200

    Full commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Navigation from "./components/Navigation";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Signup from "./components/Signup";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Navigation />
+			<Routes>
+				<Route path='/' element={<Home />} />
+
+				{/* <Route path='/about' element={<ProtectedRoute>
+												<About />
+												</ProtectedRoute>
+											} /> */}
+
+				<Route element={<ProtectedRoute/>}>
+					<Route path="/profile" element={<Profile/>}/>
+				</Route>
+
+				<Route path='/signup' element={<Signup/>}/>
+				<Route path="/login" element={<Login/>}/>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
